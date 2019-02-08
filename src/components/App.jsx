2dcbe00848b47912a2b51f7173f69341f488eb5a
heathertill <heather.till@charter.net>
2019-02-08 14:37:27 -0600

    phase 1 complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import FilmCard from './FilmCard';
 
-const App = (props) => {
-    return ( 
-        <h1>Hello World!!</h1>
-     );
+class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         films: []
+      };
+   }
+
+   async componentDidMount() {
+      let res = await fetch('https://ghibliapi.herokuapp.com/films');
+      let data = await res.json();
+      this.setState({ films: data });
+   }
+
+   render() {
+      return (
+         <div className="container">
+            <div className="container ">
+               <div className="row justify-content-center">
+                  <div className="col-2">
+                     <img
+                        src="https://ghibliapi.herokuapp.com/images/logo.svg"
+                        alt="placeholder"
+                        className="ghibli-logo"
+                     />
+                  </div>
+               </div>
+            </div>
+
+            <h1 className="title text-center m-3">
+               My Favorite Ghibli Movies!!!
+            </h1>
+            <div className="row">
+               {this.state.films.map(film => {
+                  return <FilmCard key={film.id} film={film} />;
+               })}
+            </div>
+         </div>
+      );
+   }
 }
- 
-export default App
+
+export default App;
---
+++ src/components/FilmCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const FilmCard = props => {
+   return (
+      <div className="card-deck col-md-4 ">
+         <div className="card m-2 p-1 border-dark rounded">
+            <div className="card-header border-dark">
+               <h4 className="card-title">{props.film.title}</h4>
+            </div>
+            <div className="card-body">
+               <p className="card-text">{props.film.description}</p>
+            </div>
+            <div className="card-footer bg-transparent d-flex">
+               <img
+                  className="d-flex align-items-center pr-2"
+                  style={{ height: 37, width: 57 }}
+                  src="http://chittagongit.com//images/rotten-tomatoes-icon/rotten-tomatoes-icon-21.jpg"
+                  alt="placeholder"
+               />
+               <h5 className="pt-2">{props.film.rt_score}</h5>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default FilmCard;
